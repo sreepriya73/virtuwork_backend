3f661e359a0bd849ad2d42ec58e107d993c71728
sreepriya73 <sreepriyaillikkatt@gmail.com>
2025-03-12 13:45:59 +0530

    completed

--- models/User.js
+++ models/User.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  emailid: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  emailid: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
   role: { 
     type: String, 
     enum: ["Client", "Freelancer"], // Enum with "client" and "freelancer"
     required: true 
   },
-  dob: { type: Date },
-  gender: { type: String },
-  address: { type: String },
-  district: { type: String },
-  state: { type: String },
-  qualification: { type: String },
-  bio: { type: String },
-  rewardPoints: { type: Number, default: 0 },
-}, 
-// { timestamps: true } // Uncomment if you want createdAt/updatedAt fields
-);
+  dob: { type: Date, required: true },
+  gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+  address: { type: String, required: true },
+  district: { type: String, required: true },
+  state: { type: String, required: true },
+ 
+  qualification: { type: String, required: true },
+  bio: { type: String, required: true },
+}, { timestamps: true });
 
 const usermodel = mongoose.model('User', userSchema);
 
